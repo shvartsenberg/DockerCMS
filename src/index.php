1b6729fac67256470df81f49b8a8af65c6a26c89
shvartsenberg <shvartsenberg2015@mail.ru>
2025-10-01 20:39:13 +0300

    Начал создание главной страницы

--- src/index.php
+++ src/index.php
@@ -1,3 +1,32 @@
-<?php
-phpinfo();
-?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Docker Station</title>
+    <link rel="stylesheet" href="./styles/style.css">
+</head>
+<body>
+<header class="header">
+    <a class="header-logo" href="/"><img class="header-logo-img" src="./images/ship.png" alt="Логотип"></a>
+    <ul class="header-menu">
+        <li class="header-menu-item">
+            <a href="#">Заявка на поставку</a>
+        </li>
+        <li class="header-menu-item">
+            <a href="#">Заявка на отгрузку</a>
+        </li>
+        <li class="header-menu-item">
+            <a href="#">Поставки</a>
+        </li>
+        <li class="header-menu-item">
+            <a href="#">Кабинет</a>
+        </li>
+    </ul>
+</header>
+
+
+</body>
+</html>
